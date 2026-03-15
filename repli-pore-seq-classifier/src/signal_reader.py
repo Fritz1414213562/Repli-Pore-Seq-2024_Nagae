7c20fcb50a1f21712d575a8189c842f95d27aa71
Fritz NAGAE <fritz1414213562@gmail.com>
2026-03-16 02:59:32 +0900

    fix: change fast5 reader

--- repli-pore-seq-classifier/src/signal_reader.py
+++ repli-pore-seq-classifier/src/signal_reader.py
@@ -1,22 +1,23 @@
 import numpy as np
 from constant import DataConst as const
-import h5py
-import pod5
 import sys
 
-
 def _read_fast5(fname):
+	import h5py
+	import hdf5plugin
 	retval = dict()
-	with h5py.File(fname, 'r') as fin:
-		keys = list(fin.keys())
-		for key in keys:
-			read_id = key.lstrip("read_")
-			signal = np.array(fin[key + "/Raw/Signal"])
+	with h5py.File(fname, 'r', swmr = False) as fin:
+		for read in fin:
+#			signal = _get_signal_in_fast5(fin[read])
+			dset = fin[read]["Raw"]["Signal"]
+			signal = np.empty(dset.shape, dset.dtype)
+			dset.read_direct(signal)
+			read_id = read.lstrip('read_')
 			retval[read_id] = signal[(const.signal_outlier_lim[0] <= signal) & (signal <= const.signal_outlier_lim[1])]
 	return retval
 
-
 def _read_pod5(fname):
+	import pod5
 	retval = dict()
 	with pod5.Reader(fname) as reader:
 		for read_record in reader.reads():
@@ -32,5 +33,4 @@ def read_signal(fname):
 	elif fname.endswith(".pod5"):
 		return _read_pod5(fname)
 	else:
-		print("The format of {:s} is not supported. Please check or change file suffix.", file = sys.stderr)
-		sys.exit()
+		print("Warning: The format of {:s} is not supported. Please check or change file suffix.", file = sys.stderr)
